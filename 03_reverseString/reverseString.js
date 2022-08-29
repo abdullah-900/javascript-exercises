204965cca4d3c6d612800b34e8e671dff08ef9cc
abdullah-900 <58865017+abdullah-900@users.noreply.github.com>
2022-08-29 23:13:26 +0200

    passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+const array=word.split('');
+const key =array.reverse();
+const wo = key.slice(0,key.length)
+return wo.join('');
 };
 
 // Do not edit below this line
