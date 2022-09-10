cf30d32baa25418fabf35aa238495f8c2efc4018
abdullah-900 <58865017+abdullah-900@users.noreply.github.com>
2022-09-10 22:07:26 +0200

    passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  const temp= (f-32) * (5/9)
+const rounded = Math.round(temp * 10)/10
+return rounded;
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  const temp = c *(9/5) +32
+  const rounded = Math.round(temp * 10)/10
+return rounded
 };
 
 // Do not edit below this line
