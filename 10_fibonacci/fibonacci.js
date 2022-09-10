71c2beaca35aece1ab670898c8e5274216839dbd
abdullah-900 <58865017+abdullah-900@users.noreply.github.com>
2022-09-10 22:15:58 +0200

    passed all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if (number <0) return 'OOPS'
+    const array=[1,1]
+    for (i=2;i<100;i++) {
+        array[i]=array[i-1]+array[i-2]
 
+    }
+return array[number-1]
 };
 
 // Do not edit below this line
