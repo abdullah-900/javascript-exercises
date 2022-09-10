70e5123613d0b0df0b0bf2206f59f4ea8ba99e45
abdullah-900 <58865017+abdullah-900@users.noreply.github.com>
2022-09-11 00:06:06 +0200

    passed all tests using array and reduce method

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,21 +1,26 @@
 const sumAll = function(num1,num2) {
-    if (num1>0 && num2>0) {
-
-    }else if (num1==="" || num2==="") {
-     }else 
-     { return 'ERROR'} 
+   
+   if (num1<0 || num2<0) return 'ERROR'
+   if (!Number.isInteger(num1) || !Number.isInteger(num2)) return 'ERROR'
+   const array=[]
+   if (num2>num1) {
+      for (i=0;i<num2;i++) {
+          array[i]=i+1
+       }
+       var index1=array.indexOf(num1)
+    var index2=array.indexOf(num2)
     
-    const array=[];
-    for(i=0;i<=num2;i++){
- array [i] = i
+      } else if (num2<num1) {
+      for (i=0;i<num1;i++) {
+        array [i]=i+1
+      }
+      var index1=array.indexOf(num2)
+    var index2=array.indexOf(num1)
+    
     }
-    const index =array.indexOf (num1)
-    const last=array.slice (index)
+const array2=array.slice(index1,index2+1)
 
- const totl=last.reduce(function(first,total){
-    return first+total;
-})
-return totl;
+ return array2.reduce(function(total,value){return total+value},0)
 };
 
 // Do not edit below this line
