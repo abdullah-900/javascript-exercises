d7a5d78d65ba491ecb716035a5bbf118a1b252cd
abdullah-900 <58865017+abdullah-900@users.noreply.github.com>
2022-09-10 22:53:43 +0200

    PASS

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function(array,value) {
-    
+const removeFromArray = function(array,value1,value2,value3,value4) {
+    var val1=value1
+    var val2 =value2
+    var val3 =value3
+    var val4 =value4
+    return array.filter(function(value) { return value!==val1 && value!==val2 && value!==val3 && value!==val4
+
+    })
 };
 
 // Do not edit below this line
