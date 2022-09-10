56b7bf11c79a6ce2df218a91e92afd7ddf62408c
abdullah-900 <58865017+abdullah-900@users.noreply.github.com>
2022-09-10 22:11:22 +0200

    passed all tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
-};
-
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const add =(a,b)=>{
+  return a+b;
 
 };
 
-const power = function() {
+const subtract = (a,b)=> {
+  return a-b;
 	
 };
 
-const factorial = function() {
+const sum = (arr)=> {
+  return  arr.reduce((total,value)=> total+value,0)
 	
+};
+
+const multiply = (arr)=> {
+    return arr.reduce((total,value)=> total*value,1)
+
+};
+
+const power = (a,b)=> {
+	return Math.pow(a,b);
+};
+
+const factorial = (a)=> {
+  const arr=[];
+	for (i = 0; i <= a; i++) {
+    arr[i] = i
+  }
+  arr.splice(0, 1)
+  return arr.reduce((total,value)=> total*value,1)
+  
 };
 
 // Do not edit below this line
